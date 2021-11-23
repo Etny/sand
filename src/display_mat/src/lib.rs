684f4c18ec9f149fe00a616723cb049b08074f21
Etny <ynteveeman@gmail.com>
2021-11-23 18:28:15 +0100

    minor tweaks for led matrix testing

--- src/display_mat/src/lib.rs
+++ src/display_mat/src/lib.rs
@@ -1,18 +1,20 @@
 extern crate rpi_led_matrix;
-
-use std::thread;
-
-use rpi_led_matrix::{LedMatrix, LedColor};
+use rpi_led_matrix::{LedMatrix, LedColor, LedMatrixOptions, LedRuntimeOptions};
 
 pub fn draw() {
-    let matrix = LedMatrix::new(None, None).unwrap();
+    let mut mat_opts = LedMatrixOptions::new();
+    let mut run_opts = LedRuntimeOptions::new();
+
+    mat_opts.set_cols(64);
+    run_opts.set_gpio_slowdown(1);
+
+    let matrix = LedMatrix::new(Some(mat_opts), Some(run_opts)).unwrap();
     let mut canvas = matrix.offscreen_canvas();
     for red in 0..255 {
         for green in 0..255 {
             for blue in 0..255 {
                 canvas.fill(&LedColor { red, green, blue });
                 canvas = matrix.swap(canvas);
-                thread::sleep(std::time::Duration::from_millis(10));
             }
         }
     }
